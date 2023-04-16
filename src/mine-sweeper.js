73f0387dffaf3003106ac566d57dc08a5f42b0e5
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-04-16 15:37:19 +0500

    min sweeper solve

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
+  rows = matrix.length;
+  columns = matrix[0].length;
+
+  let array = Array(rows).fill().map(() => Array(columns).fill(0));
+
+  for(let i = 0; i < rows; i++){
+    for(let j = 0; j < columns; j++){
+      if(matrix[i][j] == true) {
+        array[i][j] = 1;
+      }
+      else {
+        if(j > 0 && matrix[i][j-1] == true){
+          array[i][j] = array[i][j] + 1;
+        }
+        if(j < (columns - 1) && matrix[i][j+1] == true){
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i > 0 && matrix[i-1][j] == true){
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i < (rows - 1) && matrix[i+1][j] == true){
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i > 0 && j > 0 && matrix[i-1][j-1]){
+          if(array[i][j] != true)
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i < (rows - 1) && j < (columns - 1) && matrix[i+1][j+1]){
+          if(array[i][j] != true)
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i > 0 && j < (columns - 1) && matrix[i-1][j+1]){
+          if(array[i][j] != true)
+          array[i][j] = array[i][j] + 1;
+        }
+        if(i < (rows - 1) && j > 0 && matrix[i+1][j-1]){
+          if(array[i][j] != true)
+          array[i][j] = array[i][j] + 1;
+        }
+      }
+    }
+  }
+
+  return array;
+
 }
 
 module.exports = {

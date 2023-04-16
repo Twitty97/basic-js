bc7a575d349c80d05eb00f1aa596d633dc1745a8
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-04-16 07:58:39 +0500

    undo last program

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,16 +10,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(str) {
-  let counts = {};
-  chunk = str.split("");
-  chunk.forEach(function(x){counts[x] = (counts[x] || 0) + 1});
-  var result = Object.keys(counts).map((key) => [key, counts[key]]);
-  result.forEach(element => {
-    element = element.reverse();
-  })
-  result = result.flat(1).join("");
-  return result;
+function encodeLine(/*str*/) {
+  throw new NotImplementedError('Not implemented');
 }
 
 module.exports = {

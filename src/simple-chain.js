595cb9565039a99acd4c965d0258e506c049ab7c
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-04-16 20:41:04 +0500

    solve simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  str: '',
+  n_str: '', 
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chunk = chainMaker.str.split("~~");
+    let length = chunk.length;
+    return length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+      if(chainMaker.str == '')
+      chainMaker.str = chainMaker.str + "( " + value + " )";
+      else
+      chainMaker.str = chainMaker.str + "~~" + "( " + value + " )";
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    arr = chainMaker.str.split("~~");
+    if(arr.indexOf(arr[position-1]) < 0 ){
+      this.str= '';
+      throw Error ("You can\'t remove incorrect link!");
+    }
+
+    else {
+      arr = arr.filter(item => item !== arr[position-1]);
+      chainMaker.str = arr.join("~~");
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    arr = chainMaker.str.split("~~");
+    arr = arr.reverse();
+    chainMaker.str = arr.join("~~");
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    chainMaker.n_str = chainMaker.str;
+    chainMaker.str = '';
+    return this.n_str;
   }
 };
 

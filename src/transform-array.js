47b540d4e9b8d286644233bb2b6239033e5878ac
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-04-17 15:28:40 +0500

    array half solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+  if(Array.isArray(arr)){
+  let newArray = [];
+  let result = [];
+  let empty = [];
+  let flag = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            flag = true;
+            if(i < (arr.length - 1)){
+            result = arr.slice(0, i) + "," + arr.slice(i + 2);
+            str = result.split(",");
+            const arrOfNum = str.map(s => {return parseInt(s, 10);});
+            newArray.push(arrOfNum);
+            newArray = newArray.flat(1);
+            }
+            else {
+              result = arr.slice(0, arr.length-1);
+              newArray = result;
+            }
+        } if (arr[i] === '--discard-prev') {
+            flag = true;
+            if(i>0){
+            result = arr.slice(0, i - 1) + "," + arr.slice(i + 1);
+            str = result.split(",");
+            const arrOfNum = str.map(s => {return parseInt(s, 10);});
+            newArray.push(arrOfNum);
+            newArray = newArray.flat(1);
+            }
+            else{
+              result = arr.slice(1);
+              newArray = result;
+            }
+        } if (arr[i] === '--double-next') {
+            flag = true;
+            if(i < arr.length - 1){
+            result = arr.slice(0, i) + "," + arr[i + 1] + "," + arr.slice(i + 1);
+            str = result.split(",");
+            const arrOfNum = str.map(s => {return parseInt(s, 10);});
+            newArray.push(arrOfNum);
+            newArray = newArray.flat(1);
+            }
+            else {
+              result = arr.slice(0, arr.length-1);
+              newArray = result;
+            }
+        } if (arr[i] === '--double-prev') {
+            flag = true;
+            if(i>0){
+            result = arr.slice(0, i) + "," + arr[i - 1] + "," + arr.slice(i + 1);
+            str = result.split(",");
+            const arrOfNum = str.map(s => {return parseInt(s, 10);});
+            newArray.push(arrOfNum);
+            newArray = newArray.flat(1);
+            }
+            else {
+              result = arr.slice(1);
+              newArray = result;
+            }
+        }
+    }
+    if(flag != true){
+      return arr;
+    }
+    else{
+      empty = newArray;
+      newArray = [];
+      return empty;
+    }
+  }
+  else{
+    throw Error ("\'arr\' parameter must be an instance of the Array!");
+  }
+};
 
 module.exports = {
   transform

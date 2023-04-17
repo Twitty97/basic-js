1d6b0bc0aabfc1da5f8b5028cfbbe448abd545ad
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-04-17 10:55:53 +0500

    repeater solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  str = `${str}`;
+  if(str == null){
+    str = str.toString();
+  }
+
+  let newStr = [];
+  let addStr = [];
+
+
+  // handle object coersion 
+
+// const obj = {
+//     [Symbol.toPrimitive](hint) {
+//     if (hint !== 'number') {
+//       return 'STRING_OR_DEFAULT';
+//     }
+//     return 'NUMBER';
+//   }
+// };
+
+  // if(typeof(str) == 'object' && Object.keys(str).length == 0){
+  //   str = obj;
+  // }
+  // else {
+  //   str = str.toString();
+  // }
+
+
+  // handle default values
+
+  Object.defineProperty(options, 'separator', {
+    value: options.separator || '+', 
+  })
+
+  Object.defineProperty(options, 'additionSeparator', {
+    value: options.additionSeparator || '|', 
+  })
+
+  // functional requirements 
+
+  if(options.hasOwnProperty('addition')){
+    addStr = `${options.addition}`;
+    if(addStr == null){
+      addStr = addStr.toString();
+    }
+    // if(typeof(addStr) == 'object' && Object.keys(addStr).length == 0){
+    //   addStr = obj;
+    // }
+    // else {
+    //   addStr = addStr.toString();
+    // }
+    if(options.hasOwnProperty('additionRepeatTimes')){
+      newStr = new Array(options.additionRepeatTimes).fill(addStr).join(options.additionSeparator);
+      newStr = str + newStr;
+      str = newStr;
+    }
+    else{
+      newStr = str + addStr;
+      str = newStr;
+    }
+  }
+
+  newStr = new Array(options.repeatTimes).fill(str).join(options.separator);
+
+  return newStr;
 }
 
 module.exports = {
